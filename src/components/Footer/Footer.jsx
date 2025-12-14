1ee127ee1c5c9aa87f7c1e006d5b85923f6b8a56
Osvaldobrito <osvaldooabn@gmail.com>
2025-12-14 11:12:37 -0400

    Atualização do footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,23 @@
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__adress">
+        <img
+          className="footer__icon adress__icon"
+          src="./src/images/adress-icon.png"
+        />
+        <p className="footer__description">
+          Rua do Genipapeiro, 970 - Caçari<br></br>Boa Vista - RR
+        </p>
+      </div>
+      <div className="footer__instagram">
+        <img className="footer__icon" src="./src/images/instagram-icon.png" />
+        <p className="footer__description">@clinicajussarabarbosa</p>
+      </div>
+      <div className="footer__whatsapp">
+        <img className="footer__icon" src="./src/images/whatsapp-icon.png" />
+        <p className="footer__description">(95) 99967-7633</p>
+      </div>
+    </footer>
+  );
 }
--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -26,9 +26,9 @@ export default function Team() {
         </button>
         <div className="team__cards" ref={trackRef}>
           <div className="team__card">
-            <img className="person__photo" src="./src/images/gabi.png" />
-            <h4 className="person__name">Gabrielle Brito</h4>
-            <p className="person__document">CRP 20/12029</p>
+            <img className="person__photo" src="./src/images/jussara.png" />
+            <h4 className="person__name">Jussara Barbosa</h4>
+            <p className="person__document">CRP ...</p>
             <p className="person__skills">
               Neuropsicóloga<br></br>TCC
             </p>
