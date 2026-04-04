32b59691432cd10e7b0df6058285b4c4d4e8acf8
Osvaldobrito <osvaldooabn@gmail.com>
2026-04-04 11:06:34 -0400

    atualização do favicon e título do site

--- src/components/WhatsApp/WhatsButton.jsx
+++ src/components/WhatsApp/WhatsButton.jsx
@@ -3,7 +3,7 @@ import whatsIcon from "../../images/whatsapp-icon.svg";
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/5595999677633?text=Olá,%20gostaria%20de%20agendar%20um%20atendimento"
+      href="https://wa.me/5595999677633?text=Olá,%20vim%20do%20site%20e%20gostaria%20de%20agendar%20um%20atendimento"
       target="_blank"
       rel="noopener noreferrer"
       className="whatsapp-float"
